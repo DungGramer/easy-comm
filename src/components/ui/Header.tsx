6eefaf1fc7892fff05bc64e67834493ac4c6a43f
DungGramer <dung.dev.gramer@gmail.com>
2024-06-19 00:29:41 +0700

    update logo

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <div>
         <header className='header2023 js-header js-header-mobile'>
           <div className='header2023__inner'>
-            <div className='header2023__left'>
-              <a href='/' className='header2023__logo' />
+            <div className='header2023__left' onClick={() => navigate("/")}>
+              <img src='/logo-dark.svg' className='w-[40px] cursor-pointer' alt='Logo' />
             </div>
             <div className='header2023__center'>
               <nav className='header-menu'>
