d5808f245be81975ca82929035150660593f27dc
DungGramer <dung.dev.gramer@gmail.com>
2024-06-20 18:35:52 +0700

    Update H5 model

--- src/components/HandDetector.tsx
+++ src/components/HandDetector.tsx
@@ -9,7 +9,7 @@ import { Heading } from "@radix-ui/themes";
 
 const loadModel = async () => {
   try {
-    const modelPath = "/old-model/model.json";
+    const modelPath = "/h5-model/model.json";
     const model = await tf.loadLayersModel(modelPath);
     console.log(model.summary());
     return model;
