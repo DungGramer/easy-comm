f8fc58ba2bc0f4ed12f4206bf6a4e3df9e87f989
DungGramer <dung.dev.gramer@gmail.com>
2024-07-06 02:27:20 +0700

    add image

--- src/components/SpeechInput.tsx
+++ src/components/SpeechInput.tsx
@@ -1,45 +1,66 @@
 import { Button } from "@radix-ui/themes";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+// const labelsData = require("../../public/images/label.json").data; // Assuming the JSON file is in the same directory
 
 window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition || null; // Check if browser supports speech recognition
+  window.SpeechRecognition || window.webkitSpeechRecognition || null;
 const recognition = new SpeechRecognition();
 recognition.continuous = true;
-recognition.interimResults = true; // Set interim results to true to get interim results
+recognition.interimResults = true;
+// recognition.lang = "vi-VN"; // Set the language to Vietnamese
 
 const SpeechInput = ({ onTranscript }) => {
   const [listening, setListening] = useState(false);
   const [transcript, setTranscript] = useState("");
-  const transcriptRef = React.useRef(null);
-  const p = useRef(document.createElement("p"));
-
-  // recognition.lang = "vi-VN"; // Set the language to Vietnamese
-  recognition.onerror = (event) => console.log(event); // Log errors
+  const [imageSrc, setImageSrc] = useState("");
+  const transcriptRef = useRef(null);
+  const [labelsData, setLabelsData] = useState([]);
+  const [words, setWords] = useState(["hai"]);
 
   useEffect(() => {
-    transcriptRef.current.appendChild(p.current);
-  }, [])
+    fetch("/images/label.json")
+      .then((response) => response.json())
+      .then((x) => setLabelsData(x.data));
 
-  recognition.addEventListener("result", (e) => {
-    const transcript = Array.from(event.results)
-      .map((e) => e[0])
-      .map((e) => e.transcript)
-      .join("");
-    console.log(`📕 transcript - 16:SpeechInput.tsx \n`, transcript);
-    setTranscript(transcript);
-    if (transcriptRef.current) {
-      p.current.textContent = transcript; // Update the text in the div
+    recognition.onresult = (event) => {
+      const currentTranscript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      setTranscript(currentTranscript);
+      transcriptRef.current.textContent = currentTranscript;
 
-      // if (e.results[0].isFinal) {
-      //   p.current = document.createElement("p"); // Create a new paragraph element
-      //   transcriptRef.current.appendChild(p.current);
+      const newWords = currentTranscript.trim().split(" ");
+      const uniqueNewWords = newWords.filter((word) => !words.includes(word));
+      setWords([...words, ...uniqueNewWords]);
+
+      const foundLabel = labelsData.find((label) =>
+        label.labels.includes((uniqueNewWords.at(-1) || "").toLowerCase())
+      );
+
+      if (
+        foundLabel
+        // !imagesToShow.some((image) => image.src === foundLabel.src)
+      ) {
+        setImageSrc(`/images/${foundLabel?.src}`);
+      }
+      // if (foundLabel) {
+      //   setImageSrc(`/images/${foundLabel?.src}`);
       // }
-    }
 
-    onTranscript(transcript);
-  });
+      if (event.results[0].isFinal) {
+        onTranscript(currentTranscript);
+      }
+    };
 
-  recognition.addEventListener("end", recognition.start);
+    recognition.onerror = (event) => console.log(event);
+    recognition.onend = recognition.start;
+
+    return () => {
+      recognition.stop();
+    };
+  }, [labelsData, onTranscript, words]);
 
   const toggleListening = () => {
     if (listening) {
@@ -55,7 +76,8 @@ const SpeechInput = ({ onTranscript }) => {
       <Button onClick={toggleListening}>
         {listening ? "Stop Listening" : "Start Listening"}
       </Button>
-      <div ref={transcriptRef} className="rounded"></div>
+      <div ref={transcriptRef} className='rounded'></div>
+      {imageSrc && <img src={imageSrc} alt='Spoken Word Visualization' />}
     </section>
   );
 };
